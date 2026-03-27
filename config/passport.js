ff884bf6aa4877d94433b08d2b34fb8806ccc3bf
SpencerQuiner <spencerquiner@gmail.com>
2026-03-27 13:13:32 +0000

    update render callback url

--- config/passport.js
+++ config/passport.js
@@ -5,7 +5,7 @@ const User = require('../models/user');
 passport.use(new GitHubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: '/auth/github/callback'
+    callbackURL: 'https://cse341-week3-4-project.onrender.com/auth/github/callback'
 },
 async (accessToken, refreshToken, profile, done) => {
     try {
