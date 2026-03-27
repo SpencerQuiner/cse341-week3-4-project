cedf511bb753e59003cf149ee5650cdd7fa1abd8
SpencerQuiner <spencerquiner@gmail.com>
2026-03-27 13:46:41 +0000

    Oauth bug update

--- config/passport.js
+++ config/passport.js
@@ -28,7 +28,7 @@ passport.serializeUser((user, done) => {
     done(null, user.id);
 });
 
-passport.deserializeUser(async (id) => {
+passport.deserializeUser(async (id, done) => {
     try {
         const user = await User.findById(id);
         done(null, user);
